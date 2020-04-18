8e95babfa1ef6663da132bbe925c932791618701
nolaneo <eoin.nolan@intercom.io>
2020-04-18 15:35:12 +0100

    dislay metric

--- vemos-extension/app/services/metrics-service.js
+++ vemos-extension/app/services/metrics-service.js
@@ -1,16 +1,21 @@
 import Service, { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
 
 export default class MetricsService extends Service {
   @service parentDomService;
   vemosMetricsFrame = undefined;
   queuedMetrics = [];
+  @tracked version;
 
   initialize(frame) {
     this.vemosMetricsFrame = frame;
     this.queuedMetrics.forEach((metric) => this.recordMetric(metric));
+    this.version = document
+      .querySelector("#vemos-version-number")
+      ?.getAttribute("content");
   }
 
-  recordMetric(metricName, data = {}) {
+  recordMetric(metricName, data = {}, version) {
     if (this.vemosMetricsFrame) {
       try {
         console.log("Event: ", metricName);
@@ -19,6 +24,7 @@ export default class MetricsService extends Service {
             vemos_event: metricName,
             metadata: {
               ...data,
+              vemos_version: this.version,
               host: this.parentDomService.host,
             },
           },
