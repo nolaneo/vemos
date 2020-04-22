ace620422582dda69d4774ad15295fcc4ddfa5f6
nolaneo <eoin.nolan@intercom.io>
2020-04-22 09:49:15 +0100

    log service

--- vemos-extension/app/services/peer-service.js
+++ vemos-extension/app/services/peer-service.js
@@ -35,6 +35,7 @@ export { RTCMessage };
 
 export default class PeerService extends Service {
   @service metricsService;
+  @service logService;
 
   @tracked peer = undefined;
   @tracked peerId = undefined;
@@ -42,10 +43,11 @@ export default class PeerService extends Service {
   @tracked currentMediaStream;
 
   eventHandlers = {};
+  reconnectionAttempts = {};
   knownEvents = A();
 
   initialize() {
-    console.log(`Initializing Peer Service`);
+    this.logService.log();
     this.peer = new Peer({
       host: HOST,
     });
@@ -77,7 +79,7 @@ export default class PeerService extends Service {
   }
 
   sendRTCMessage(message) {
-    console.log(
+    this.logService.log(
       `Sending message [event ${message.event} | uuid ${message.uuid}]`
     );
     message.senderId = this.peerId;
@@ -87,12 +89,14 @@ export default class PeerService extends Service {
   }
 
   onPeerOpen(id) {
-    console.log("onPeerOpen", id);
+    this.logService.log(
+      `Connection to signaling server established. Our ID is ${id}`
+    );
     this.peerId = id;
   }
 
   onPeerConnection(connection) {
-    console.log("onPeerConnection", connection);
+    this.logService.log(`A peer connected. Their ID is ${connection.peer}`);
     let message = new RTCMessage({
       event: "new-peer-joined",
       data: {
@@ -107,10 +111,13 @@ export default class PeerService extends Service {
 
   onPeerDisconnected() {
     console.log("onPeerDisconnected");
+    this.logService.log(`You have disconnected, attempting to reconnect`);
+    this.peer.reconnect();
   }
 
   onPeerError(error) {
     console.log("onPeerError", error.type, error);
+    this.logService.error(`An error occurred: ${error.message}`);
     this.metricsService.recordMetric("on-peer-error", { type: error.type });
   }
 
@@ -137,9 +144,10 @@ export default class PeerService extends Service {
   }
 
   onConnectionOpen(connection) {
-    console.log("onConnectionOpen");
+    this.logService.log(`Connection established with ${connection.peer}`);
     connection.on("data", this.onConnectionData.bind(this, connection));
     connection.on("close", this.onConnectionClose.bind(this, connection));
+    connection.on("error", this.onConnectionError.bind(this, connection));
 
     if (this.eventHandlers["connection-opened"]) {
       this.eventHandlers["connection-opened"].forEach((handler) =>
@@ -149,16 +157,15 @@ export default class PeerService extends Service {
   }
 
   onConnectionData(connection, message) {
-    console.log("onConnectionData", connection, message);
     if (this.knownEvents.includes(message.uuid)) {
       console.log(`Ignoring known event`, event.uuid);
       return;
     }
     this.knownEvents.pushObject(message.uuid);
     if (this.eventHandlers[message.event]) {
-      console.log(
-        `Received message [event ${message.event} | uuid ${message.uuid}]`
-      );
+      this.logService.log(`Received message [event ${message.event}`);
+
+      console.log(` | uuid ${message.uuid}]`);
       this.eventHandlers[message.event].forEach((handler) => handler(message));
     } else {
       console.log(`No event handlers for ${message.event}`);
@@ -168,11 +175,29 @@ export default class PeerService extends Service {
   onConnectionClose(connection) {
     this.connections.removeObject(connection);
     console.log("onConnectionClose");
-
+    this.logService.log(
+      `A peer has closed their connection ${connection.peer}`
+    );
     if (this.eventHandlers["connection-closed"]) {
       this.eventHandlers["connection-closed"].forEach((handler) =>
         handler(connection)
       );
+    }
+  }
+
+  onConnectionError(connection) {
+    console.log("onConnectionError");
+    this.logService.error(
+      `Connection with peer has been lost. ${connection.peer}`
+    );
+    if (this.reconnectionAttempts[connection.peer] > 5) {
+      console.log("Giving up on peer");
+    } else {
+      this.connectToPeer(connection.peer);
+      this.reconnectionAttempts[connection.peer] = this.reconnectionAttempts[
+        connection.peer
+      ] = this.reconnectionAttempts[connection.peer] | 0;
+      this.reconnectionAttempts[connection.peer] += 1;
     }
   }
 
